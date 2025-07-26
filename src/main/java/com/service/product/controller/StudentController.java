97cf6c140ab5e3ff37321d9a643d0a47d8d2580b
Manisha <manisharajan1609@gmail.com>
2025-07-26 15:00:33 +0530

    Commit1

---
+++ src/main/java/com/service/product/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.service.product.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/student")
+public class StudentController {
+	@GetMapping("/display")
+	public String display() {
+		return "<h1>Hi</h1>";
+	}
+
+}
